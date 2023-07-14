3416136034908aa5e7c7e2c54ab76a8a4ed101f2
yamini0984 <107943136+yamini0984@users.noreply.github.com>
2023-07-14 15:26:40 +0530

    Update timeseries.py

--- timeseries.py
+++ timeseries.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import pickle
+
+# Set the page configuration with the title and background image
+st.set_page_config(
+    page_title="Apple Stock Price Prediction",
+    image:"apple1images.jpg",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 emamodel=pickle.load(open('./model.pkl','rb'))
 startDate=st.date_input("Start Date")
 endDate=st.date_input("End Date")

0d04649e659afc72a53b21313726319623dabb70
yamini0984 <107943136+yamini0984@users.noreply.github.com>
2023-07-14 15:42:38 +0530

    Update timeseries.py

--- timeseries.py
+++ timeseries.py
@@ -1,24 +1,6 @@
 import streamlit as st
 import pickle
 
-# Set the page configuration with the title and background image
-html_temp="""
-<div style ="background-color:grey;padding:10px">
-<h2 style="color:white;text-align:center;">Apple Stock Price Prediction </h2>
-"""
-st.markdown(html_temp,unsafe_allow_html=True)
-
-def center_image(image_path):
-    st.markdown(
-        f'<div style="display: flex; justify-content: center;"><img src="{image_path}" width="100; max-height: 100%;"/></div>',
-        unsafe_allow_html=True
-    )
-    
-# Usage
-center_image('apple1images.jpg')
-image = Image.open("apple1images.jpg")
-st.image(image)
-
 emamodel=pickle.load(open('./model.pkl','rb'))
 startDate=st.date_input("Start Date")
 endDate=st.date_input("End Date")

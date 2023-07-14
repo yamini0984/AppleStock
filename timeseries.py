92b641eaff7f774b3d4bf0e5da86e608f20f9f5d
yamini0984 <107943136+yamini0984@users.noreply.github.com>
2023-07-14 13:03:31 +0530

    Delete timeseries.py

--- timeseries.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pickle
-emamodel=pickle.load(open('./model(1).pkl','rb'))
-startDate=st.date_input("Start Date")
-endDate=st.date_input("End Date")
-if st.button("predict"):
-    prediction=emamodel.predict(startDate,endDate)
-    st.line_chart(prediction)
-
